21a06f450bfa787c7bd18f4f752384d60c76060f
Ingrid Banguero <ingrid_banguero@hotmail.com>
2021-02-21 10:15:41 -0500

    currency: create template for currency section and add styles

---
+++ index.js
@@ -0,0 +1,13 @@
+let tableArrowLeft = document.getElementById("arrowLeft");
+let tableArrowRight = document.getElementById("arrowRight");
+let tableCurrency = document.getElementById("tableCurrency");
+let tableCommissions = document.getElementById("tableCommissions");
+
+tableArrowRight.addEventListener("click", () => {
+    tableCurrency.style.display = "none";
+    tableCommissions.style.display = "block";
+})
+tableArrowLeft.addEventListener("click", () => {
+    tableCurrency.style.display = "block";
+    tableCommissions.style.display = "none";
+})
